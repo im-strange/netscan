cbab0ed764f4121a1d6c8fb1b19fb0c6284053b7
im-strange <itsmestrange.dump@gmail.com>
2023-05-04 13:01:57 +0800

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-
 from setuptools import setup
 
 setup(
   name="netscan",
   version="1.0",
+  author="Sam Genoguin",
+  description="A Python tool for network scanning.",
+  url="https://github.com/im-strange/netscan.git",
+  install_requires=[
+    "threading",
+    "socket",
+    "time",
+    "re",
+    "sys",
+    "tqdm",
+    "requests"
+  ]
   py_modules=["netscan"],
   entry_points={
     "console_scripts": [
